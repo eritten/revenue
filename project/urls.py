bcb9ddba26b0692fc0d79c793199d8ec780d1b15
= <=>
2023-04-29 09:57:24 -0200

    adding changes

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from revenue import views
+from home.views import  home
 
 urlpatterns = [
+path("", home, name="home"),
 path("incoming/", views.incoming, name="incoming"),
 path("outgoing/", views.outgoing, name="outgoing"),
 path("dashboard/", views.dashboard, name="dashboard"),
