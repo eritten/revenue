6506f528ce2f8ede0a88bc0266ec2dd2ed6e611c
= <=>
2023-05-02 05:56:39 -0200

    adding changes

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%34^9*+@uho-z5re%ly4$o-^2hr%f+qf*vj@gw!35y8e04t@11'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
